5ffb47168496594db87ef50156247fddef43e766
Zach Collier <zamicol@gmail.com>
2015-03-12 13:26:24 -0600

    Update 2015-03-12.solution.rs

--- 2015-03-12.solution.rs
+++ 2015-03-12.solution.rs
@@ -1,20 +1,11 @@
 # Rust solution via @jrick
 
+use std::cmp;
 use std::num::{Int, NumCast};
 
-fn percents<T: Int + NumCast>(input: &Vec<T>) -> Vec<f64> {
-    let max: T = {
-        let mut max: T = T::zero();
-        for elem in input {
-            if *elem > max {
-                max = *elem;
-            }
-        }
-        max
-    };
-    input.iter().map(|elem|
-        100f64 * f64::from::<T>(*elem).unwrap() / f64::from::<T>(max).unwrap()
-    ).collect()
+fn percents<T: Int>(input: &Vec<T>) -> Vec<f64> {
+    let mul = f64::from(input.iter().fold(T::zero(), |m, &e| cmp::max(m, e))).unwrap();
+    input.iter().map(|elem| mul * f64::from(*elem).unwrap()).collect()
 }
 
 fn main() {
